31805a9341cca487a2b2ec025938c48da0470194
nivlac <nivlac@uw.edu>
2024-10-21 14:57:43 -0700

    Finished button implementation

---
+++ app/src/main/java/edu/uw/ischool/nivlac/biggobutton/MainActivity.kt
@@ -0,0 +1,40 @@
+package edu.uw.ischool.nivlac.biggobutton
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.bigButton)
+        var clicked: Int = 0
+
+        button.setOnClickListener {
+            clicked++
+            button.setText(when(clicked){
+                1 -> "You have pushed me 1 time!"
+                else -> "You have pushed me $clicked times!"
+            })
+
+            button.setBackgroundColor(Color.rgb(Random.nextInt(255), Random.nextInt(255),
+                Random.nextInt(255)))
+            button.setTextColor(Color.rgb(Random.nextInt(255), Random.nextInt(255),
+                Random.nextInt(255)))
+        }
+
+    }
+}
